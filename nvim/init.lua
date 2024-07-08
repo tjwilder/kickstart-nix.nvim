826f22c6cff2af9e4765e25b40fc137e9341f864
TJ <tj@wilder.tj>
2024-07-07 22:55:58 -0500

    unnamed buffer

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,6 +41,7 @@ opt.undofile = true
 opt.splitright = true
 opt.splitbelow = true
 opt.cmdheight = 0
+opt.clipboard = 'unnamed'
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
